156e5eb20f638d392ee5f3e1cc51907eba37f785
carlosromerorodriguez <c.romero@students.salle.url.edu>
2024-04-23 00:19:04 +0200

    [FILE PATHS] Make them global

--- ProjecteLlenguatges/src/Main.java
+++ ProjecteLlenguatges/src/Main.java
@@ -5,10 +5,13 @@ import java.util.Map;
 
 
 public class Main {
+    private static final String FILE_PATH = "src/files/example1.ç";
+    private static final String GRAMMAR_PATH = "src/files/grammar.json";
+
     public static void  main(String[] args) {
         TokenConverter tokenConverter = new TokenConverter();
         ErrorHandler errorHandler = new ErrorHandler();
-        PreProcessing preProcessing = new PreProcessing(errorHandler, "src/files/example1.ç");
+        PreProcessing preProcessing = new PreProcessing(errorHandler, FILE_PATH);
         Lexer lexer = new Lexer(errorHandler, preProcessing.readFile(), tokenConverter);
 
         lexer.showTokens();
@@ -16,7 +19,7 @@ public class Main {
         System.out.println("\n\nERRORS:");
         errorHandler.printErrors();
 
-        Map<String, List<List<String>>> grammar = preProcessing.loadGrammar("src/files/grammar.json");
+        Map<String, List<List<String>>> grammar = preProcessing.loadGrammar(GRAMMAR_PATH);
 
         Parser parser = new Parser(new FirstFollow(grammar), tokenConverter);
         parser.printParseTable();
