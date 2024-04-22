7eea15c09b4acb0d2d9227baec80f6350f5b1d98
Carlos Romero Rodríguez <c.romero@students.salle.url.edu>
2024-04-23 00:26:25 +0200

    [UNUSED] Class unused

--- ProjecteLlenguatges/src/frontEnd/Production.java
+++ ProjecteLlenguatges/src/frontEnd/Production.java
@@ -1,16 +1,1 @@
-package frontEnd;
 
-import java.util.List;
-
-public class Production {
-    private String name;
-    private List<List<String>> estructura;
-
-    public String getName() {
-        return name;
-    }
-
-    public List<List<String>> getEstructura() {
-        return estructura;
-    }
-}
