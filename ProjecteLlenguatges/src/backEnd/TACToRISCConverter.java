bf88b1e344a50289c14e7941001784458e58a721
oriol <oriol.rebordosa@students.salle.url.edu>
2024-05-15 18:02:32 +0200

    comentaris anteriors

--- ProjecteLlenguatges/src/backEnd/TACToRISCConverter.java
+++ ProjecteLlenguatges/src/backEnd/TACToRISCConverter.java
@@ -217,3 +217,88 @@ public class TACToRISCConverter {
         return "lw " + reg + ", " + offset + "($sp)";
     }
 }
+
+/*
+TAC: if a < b goto L1
+MIPS: slt $t0, $a, $b  ; set on less than
+      bne $t0, $zero, L1  ; branch if not equal to zero
+
+  TAC if i >= 10 goto L2
+        bge $t0, 10, L2
+
+TAC: a = b
+MIPS: move $a, $b  ; Suponiendo que a y b están en registros $a y $b
+
+TAC: t1 = b + c
+MIPS: add $t1, $b, $c  ; Nuevamente, suponiendo que b y c están en registros $b y $c
+
+TAC: a = M[addr]
+MIPS: lw $a, addr
+TAC: M[addr] = b
+MIPS: sw $b, addr
+
+ */
+
+/*
+
+https://www.doc.ic.ac.uk/lab/secondyear/spim/node15.html
+//https://github.com/ffcabbar/MIPS-Assembly-Language-Examples/blob/master
+//https://web.cecs.pdx.edu/~mperkows/temp/register-allocation.pdf
+
+while:
+	beq $t1,$zero,endWhile
+	rem $t3,$t1,10                                   #  123%10 = 3
+	add $t2,$t2,$t3
+	div $t1,$t1,10
+	j while
+
+    recursivePowerFunction:
+# a0 -> baseNumber       a1 -> expNumber		v0 -> result
+#	recursivePowerFunction
+#	   if (expNumber == 0)
+#			return 1;
+#	   else
+#		   return baseNumber * recursivePowerFunction(baseNumber,expNumber - 1)
+
+	bne $a1,$zero,recursion
+	li $v0,1
+	jr $ra
+recursion:
+	addi $sp,-4
+	sw $ra,0($sp)
+	addi $a1,$a1,-1
+	jal recursivePowerFunction
+	mul $v0,$v0,$a0
+	lw $ra,0($sp)
+	addi $sp,4
+	jr $ra
+
+
+#########################################################
+main:
+	li $v0, 4
+	la $a0, string1
+	syscall
+
+	li $v0,5
+	syscall
+	move $t0,$v0
+
+	li $v0, 4
+	la $a0, string2
+	syscall
+
+	li $v0,5
+	syscall
+	move $t1,$v0
+
+	move $a0,$t0
+	move $a1,$t1
+	jal recursivePowerFunction
+	move $a0,$v0
+	li $v0,1
+	syscall
+
+	li $v0, 10
+	syscall
+ */
